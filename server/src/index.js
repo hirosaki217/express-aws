912008bf41e99a11acde78862d51a79000415381
hirosaki217 <hirosaki217@gmail.com>
2022-09-19 17:12:44 +0700

    2

--- server/src/index.js
+++ server/src/index.js
@@ -9,10 +9,33 @@ app.use(express.static('./views'))
 app.set("view engine", "ejs");
 app.set('views', './views')
 
-
+const multer = require('multer')
+const upload = multer()
 
 
 const tblSanPham = "SanPham";
+
+app.post("/", upload.fields([]), (req, res) => {
+    const { ma_sp, ten_sp, so_luong } = req.body;
+
+
+    const params = {
+        TableName: tblSanPham,
+        Item: {
+            "ma_sp": ma_sp,
+            "ten_sp": ten_sp,
+            "so_luong": so_luong
+        }
+    }
+
+    docClient.put(params, (err, data) => {
+        if (err) {
+            res.send("Internal Server Error")
+        } else {
+            return res.redirect("/")
+        }
+    })
+})
 
 
 app.get("/", function(req, res) {
